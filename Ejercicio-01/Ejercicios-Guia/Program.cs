faf5338b68b62d0fd0c83008fc79cc3d0dcaa5c1
emmanueldavidmartin <emmanuel.david.martin77@gmail.com>
2023-02-25 17:43:25 -0300

    prueba commit

---
+++ Ejercicio-01/Ejercicios-Guia/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejercicios_Guia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
