d11e7256654cfce243ba8a28a31c432394de20bd
emmanueldavidmartin <emmanuel.david.martin77@gmail.com>
2023-02-25 19:45:37 -0300

    nuevo ejercicio

---
+++ Ejercicio02/Ejercicio02/Program.cs
@@ -0,0 +1,49 @@
+﻿//Ingresar un número y mostrar el cuadrado y el cubo del mismo.
+//Se debe validar que el número sea mayor que cero, caso contrario,
+//mostrar el mensaje: "ERROR. ¡Reingresar número!".
+//Utilizar el método Pow de la clase Math para calcular las potencias.
+namespace Ejercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double square;
+            double qube;
+            double number;
+            bool conditionNumber;
+     
+            do
+            {
+
+                Console.WriteLine("Ingrese un numero mayor a 0, por favor");
+                string numberString = Console.ReadLine();
+                double.TryParse(numberString, out number);
+
+
+                conditionNumber = ValidateNumber(numberString, number);
+              
+
+            } while (!conditionNumber || number < 0);
+
+            square = Math.Pow(number, 2);
+            qube = Math.Pow(number, 3);
+            Console.WriteLine("El cuadrado de {0} es {1}", number, square);
+            Console.WriteLine("El cubo de {0} es {1}", number, qube);
+
+        }
+
+        static bool ValidateNumber(string input, double number)
+        {
+         
+            if (double.TryParse(input, out number))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
